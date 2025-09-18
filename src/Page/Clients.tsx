c3ee2527ad8c799230ebabd4ebc05c16a7645119
naimsheikh8020 <naimsheikh8020@gmail.com>
2025-09-19 05:32:21 +0600

    clients page

--- src/Page/Clients.tsx
+++ src/Page/Clients.tsx
@@ -1,8 +1,21 @@
+import Button from "@/Components/Button";
+import { useLocation } from "react-router-dom";
+import { FiPlus } from "react-icons/fi";
 
 const Clients = () => {
+    const location = useLocation();
+    const currentPage = location.pathname.split("/");
     return (
-        <div>Clients</div>
+        <div className="px-4 pt-5">
+            <div className="flex justify-between">
+                <div>
+                    <h2>{currentPage}</h2>
+                    <p>Manage clients and subscription</p>
+                </div>
+                <Button icon={<FiPlus />} text="Add Client" />
+            </div>
+        </div>
     )
 }
 
-export default Clients
+export default Clients;
