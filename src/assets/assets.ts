405f9402b7791a9dd1db1375acf5b4e292022a65
naimsheikh8020 <naimsheikh8020@gmail.com>
2025-09-19 02:46:20 +0600

    Add new SVG assets and update asset management
    - Added new SVG files for Employees, Region, Setting, Subscriptions, Invoice, Map, Notification, Report, and Service.
    - Created a new assets.ts file to manage imports of the new SVG assets.
    - Removed the old react.svg file.
    - Updated index.css to ensure proper formatting.

---
+++ src/assets/assets.ts
@@ -0,0 +1,27 @@
+import logo from "./Clean_Up_logo.png"
+import Clients_icon from "./Clients.svg"
+import Dashboard_icon from "./Dashboard_icon.svg"
+import Employess_icon from "./Employees.svg"
+import invoice from "./invoice.svg"
+import map from "./map.svg"
+import notification from "./notification.svg"
+import region from "./region.svg"
+import report from "./report.svg"
+import service from "./service.svg"
+import setting from "./setting.svg"
+
+
+export const assets = {
+    logo,
+    Clients_icon,
+    Dashboard_icon,
+    Employess_icon,
+    invoice,
+    map,
+    notification,
+    region,
+    report,
+    service,
+    setting
+}
+
