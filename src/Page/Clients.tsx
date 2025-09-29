51abc4a51f0f9c6f595b4ef1007c9f317de40eec
saruf <saruf700@gmail.com>
2025-09-30 02:32:24 +0600

    feat: add shadcn dialog

--- src/Page/Clients.tsx
+++ src/Page/Clients.tsx
@@ -1,10 +1,18 @@
 import { assets } from "@/assets/assets";
-import Button from "@/Components/Button"
+import Button from "@/Components/Button";
 import Card from "@/Components/Card";
 import UserCard from "@/Components/UserCard";
 import { useEffect, useState } from "react";
 import { FaPlus } from "react-icons/fa6";
-import { IoIosSearch } from 'react-icons/io';
+import { IoIosSearch } from "react-icons/io";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/Components/ui/dialog";
 
 interface UserStats {
     revenue: number;
@@ -37,10 +45,11 @@ const Clients = () => {
             .catch((err) => console.error("Error loading users:", err));
     }, []);
 
-    const filteredUsers = users.filter((user) =>
-        user.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        user.email.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        user.location.toLowerCase().includes(searchTerm.toLowerCase())
+    const filteredUsers = users.filter(
+        (user) =>
+            user.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            user.email.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            user.location.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
     return (
@@ -50,20 +59,144 @@ const Clients = () => {
                 {/* Title */}
                 <div className="flex justify-between">
                     <div>
-                        <h2 className="text-black text-2xl font-bold">Clients</h2>
-                        <p className=" text-[#808080] text-base mt-1">Manage clients and subscription</p>
+                        <h2 className="text-black text-2xl font-bold">
+                            Clients
+                        </h2>
+                        <p className=" text-[#808080] text-base mt-1">
+                            Manage clients and subscription
+                        </p>
                     </div>
                     <div>
-                        <Button icon={<FaPlus />} text="Add Client" />
+                        <Dialog>
+                            <DialogTrigger>
+                                <Button icon={<FaPlus />} text="Add Client" />
+                            </DialogTrigger>
+                            <DialogContent
+                            className="bg-white border-0 rounded-lg"
+                            >
+                                <DialogHeader>
+                                    <DialogTitle>
+                                        New Client
+                                    </DialogTitle>
+                                    <DialogDescription>
+                                       <form>
+                                          <div className="grid grid-cols-2 gap-4 py-4">
+                                             <div className="grid grid-cols-1 items-center ">
+                                                <label
+                                                   htmlFor="name"
+                                                   className="text-left"
+                                                >
+                                                   Name<span className="text-red-500">*</span>
+                                                </label>
+                                                <input
+                                                   type="text"
+                                                   id="name"
+                                                   className="col-span-3 border border-gray-300 rounded-md p-2 w-full"
+                                                />
+                                             </div>
+                                             <div className="grid grid-cols-1 items-center">
+                                                <label
+                                                   htmlFor="email"
+                                                   className="text-left"
+                                                >
+                                                    Email<span className="text-red-500">*</span>
+                                                </label>
+                                                <input
+                                                   type="email"
+                                                   id="email"
+                                                   className="col-span-3 border border-gray-300 rounded-md p-2 w-full"
+                                                />
+                                             </div>
+                                             <div className="grid grid-cols-1 items-center">
+                                                <label
+                                                   htmlFor="phone"
+                                                   className="text-left"
+                                                >
+                                                    Phone<span className="text-red-500">*</span>
+                                                </label>
+                                                <input
+                                                   type="phone"
+                                                   id="phone"
+                                                   className="col-span-3 border border-gray-300 rounded-md p-2 w-full"
+                                                />
+                                             </div>
+                                             <div className="grid grid-cols-1 items-center">
+                                                <label
+                                                   htmlFor="address"
+                                                   className="text-left"
+                                                >
+                                                    Address<span className="text-red-500">*</span>
+                                                </label>
+                                                <input
+                                                   type="address"
+                                                   id="address"
+                                                   className="col-span-3 border border-gray-300 rounded-md p-2 w-full"
+                                                />
+                                             </div>
+                                             <div className="col-span-2 gap-2 grid grid-cols-1">
+                                                <div className="grid grid-cols-1 items-center">
+                                                   <label
+                                                      htmlFor="tags"
+                                                      className="text-left"
+                                                   >
+                                                      Tags
+                                                   </label>
+                                                   <input
+                                                      type="tags"
+                                                      id="tags"
+                                                      className="col-span-3 border border-gray-300 rounded-md p-2 w-full"
+                                                      placeholder="Type & Enter"
+                                                   />
+                                                </div>
+                                                <div className="grid grid-cols-1 items-center">
+                                                   <label
+                                                      htmlFor="buildings"
+                                                      className="text-left"
+                                                   >
+                                                      Associated Buildings
+                                                   </label>
+                                                   <input
+                                                      type="buildings"
+                                                      id="buildings"
+                                                      className="col-span-3 border border-gray-300 rounded-md p-2 w-full"
+                                                   />
+                                                </div>
+                                                </div>
+                                             </div>
+                                       </form>
+                                    </DialogDescription>
+                                </DialogHeader>
+                            </DialogContent>
+                        </Dialog>
                     </div>
                 </div>
 
                 {/* Top Card */}
                 <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4 mt-6">
-                    <Card title="Total Service" number="04" iconSrc={assets.total_service} iconAlt="An icon of a shopping cart" />
-                    <Card title="Avg Rating" number="4.6" iconSrc={assets.AVG_rating} iconAlt="An icon of a shopping cart" />
-                    <Card title="Total Revenue" number={2540000} iconSrc={assets.Total_revenue} iconAlt="An icon of a shopping cart" />
-                    <Card title="Avg Popularity" number={"85%"} iconSrc={assets.Avg_Popularity} iconAlt="An icon of a shopping cart" />
+                    <Card
+                        title="Total Service"
+                        number="04"
+                        iconSrc={assets.total_service}
+                        iconAlt="An icon of a shopping cart"
+                    />
+                    <Card
+                        title="Avg Rating"
+                        number="4.6"
+                        iconSrc={assets.AVG_rating}
+                        iconAlt="An icon of a shopping cart"
+                    />
+                    <Card
+                        title="Total Revenue"
+                        number={2540000}
+                        iconSrc={assets.Total_revenue}
+                        iconAlt="An icon of a shopping cart"
+                    />
+                    <Card
+                        title="Avg Popularity"
+                        number={"85%"}
+                        iconSrc={assets.Avg_Popularity}
+                        iconAlt="An icon of a shopping cart"
+                    />
                 </div>
 
                 {/* Search + Filters */}
@@ -80,21 +213,39 @@ const Clients = () => {
                     </div>
                     <div className="flex gap-4">
                         <div className="flex gap-2">
-                            <p className="text-base text-gray-500 py-2">Status</p>
-                            <p className="bg-green-100 p-2 text-green-600 rounded-full">Active</p>
-                            <p className="bg-yellow-100 p-2 text-yellow-600 rounded-full">Inactive</p>
-                            <p className="bg-red-100 p-2 text-red-600 rounded-full">Suspended</p>
+                            <p className="text-base text-gray-500 py-2">
+                                Status
+                            </p>
+                            <p className="bg-green-100 p-2 text-green-600 rounded-full">
+                                Active
+                            </p>
+                            <p className="bg-yellow-100 p-2 text-yellow-600 rounded-full">
+                                Inactive
+                            </p>
+                            <p className="bg-red-100 p-2 text-red-600 rounded-full">
+                                Suspended
+                            </p>
                         </div>
                         <div className="flex gap-2">
                             <p className="text-base text-gray-500 p-2">Type</p>
-                            <p className="bg-gray-100 p-2 text-gray-600 rounded-full">Individual</p>
-                            <p className="bg-gray-100 p-2 text-gray-600 rounded-full">Business</p>
+                            <p className="bg-gray-100 p-2 text-gray-600 rounded-full">
+                                Individual
+                            </p>
+                            <p className="bg-gray-100 p-2 text-gray-600 rounded-full">
+                                Business
+                            </p>
                         </div>
                         <div className="flex gap-2">
                             <p className="text-base p-2 text-gray-500">Plan</p>
-                            <p className="bg-gray-100 p-2 text-gray-600 rounded-full">Basic</p>
-                            <p className="bg-gray-100 p-2 text-gray-600 rounded-full">Premium</p>
-                            <p className="bg-gray-100 p-2 text-gray-600 rounded-full">Enterprise</p>
+                            <p className="bg-gray-100 p-2 text-gray-600 rounded-full">
+                                Basic
+                            </p>
+                            <p className="bg-gray-100 p-2 text-gray-600 rounded-full">
+                                Premium
+                            </p>
+                            <p className="bg-gray-100 p-2 text-gray-600 rounded-full">
+                                Enterprise
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -108,13 +259,14 @@ const Clients = () => {
                             <UserCard key={user.id} user={user} />
                         ))
                     ) : (
-                        <p className="text-gray-500 text-center col-span-full">No users found.</p>
+                        <p className="text-gray-500 text-center col-span-full">
+                            No users found.
+                        </p>
                     )}
                 </div>
             </div>
-
         </div>
-    )
-}
+    );
+};
 
 export default Clients;
