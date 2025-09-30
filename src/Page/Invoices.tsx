c774c485df5a426f68e019f9695bc0bcd55d56ee
naimsheikh8020 <naimsheikh8020@gmail.com>
2025-10-01 02:06:50 +0600

    Invoice page done

--- src/Page/Invoices.tsx
+++ src/Page/Invoices.tsx
@@ -1,6 +1,7 @@
 import { assets, InvoiceTopCard } from "@/assets/assets";
 import Button from "@/Components/Button";
 import Card from "@/Components/Card";
+import InvoicesList from "@/Components/InvoicesList";
 import { FaPlus } from "react-icons/fa";
 import { FiDownload } from "react-icons/fi";
 
@@ -14,49 +15,31 @@ const Invoices = () => {
           <p className="text-[#808080] text-base mt-1">Manage clients and subscription</p>
         </div>
         <div className="flex gap-2">
-          <Button icon={<FiDownload  />} text="Create Invoice" />
+          <Button icon={<FiDownload />} text="Create Invoice" />
           <Button icon={<FaPlus />} text="Export" />
         </div>
       </div>
 
       <div className="grid grid-cols-5 gap-4 mt-6">
-            {InvoiceTopCard.map((card, index) => (
-              <Card
-                key={index}
-                title={card.title}
-                number={card.number}
-                iconSrc={assets[card.iconKey]}
-                iconAlt={card.iconAlt}
-              />
-            ))}
+        {InvoiceTopCard.map((card, index) => (
+          <Card
+            key={index}
+            title={card.title}
+            number={card.number}
+            iconSrc={assets[card.iconKey]}
+            iconAlt={card.iconAlt}
+          />
+        ))}
       </div>
 
-       {/* Search Functionality */}
-      <div className="flex items-center gap-4 mt-8 p-6 bg-white border border-gray-300 rounded-xl shadow-sm">
-            <input
-              type="text"
-              placeholder="Search employees..."
-              className="border border-gray-300 rounded-md px-4 py-2 w-64 focus:outline-none focus:ring-1 focus:ring-blue-500 text-sm"
-            />
-            <select className="border border-gray-300 rounded-md px-6 py-2 text-sm text-gray-600 cursor-pointer">
-              <option>All Departments</option>
-              <option>HR</option>
-              <option>Finance</option>
-              <option>IT</option>
-            </select>
-            <select className="border border-gray-300 rounded-md px-6 py-2 text-sm text-gray-600 cursor-pointer">
-              <option>All Status</option>
-              <option>Active</option>
-              <option>On Leave</option>
-              <option>Inactive</option>
-            </select>
-            <select className="border border-gray-300 rounded-md px-6 py-2 text-sm text-gray-600 cursor-pointer">
-              <option>All Shifts</option>
-              <option>Morning</option>
-              <option>Evening</option>
-              <option>Night</option>
-            </select>
-          </div>
+      {/* Search Functionality */}
+      
+
+
+      {/*  Invoice Card */}
+      
+      <InvoicesList  />
+      
     </div>
   );
 };
