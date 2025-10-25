91cd9edcab79610944ff0252371fffd4a1e15eb8
saruf <saruf700@gmail.com>
2025-10-25 07:47:30 +0600

    feat: Enhance sidebar items for admin and employee dashboards with new routes and components

--- src/Router/sidebar-items/admin-items.tsx
+++ src/Router/sidebar-items/admin-items.tsx
@@ -1,7 +1,37 @@
 import { lazy } from "react";
+import type { ISidebarItems } from "@/Types/Types";
 const Clients = lazy(() => import("@/Page/admin-dashboard/Clients"));
 const Dashboard = lazy(() => import("@/Page/admin-dashboard/Dashboard"));
-import type { ISidebarItems } from "@/Types/Types";
+const Invoices = lazy(() => import("@/Page/admin-dashboard/Invoices"));
+const ExpenseAdmin = lazy(() => import("@/Page/admin-dashboard/ExpenseAdmin"));
+const CreateInvoiceForm = lazy(
+  () => import("@/Page/admin-dashboard/create-invoice")
+);
+const Employees = lazy(() => import("@/Page/admin-dashboard/Employees"));
+const Notifications = lazy(
+  () => import("@/Page/admin-dashboard/Notifications")
+);
+const Region = lazy(() => import("@/Page/admin-dashboard/Region"));
+const ReportsPage = lazy(() => import("@/Page/admin-dashboard/Reports"));
+const Services = lazy(() => import("@/Page/admin-dashboard/Services"));
+const ProfileSettingsPage = lazy(
+  () => import("@/Page/admin-dashboard/Settings")
+);
+const Subscriptions = lazy(
+  () => import("@/Page/admin-dashboard/Subscriptions")
+);
+const SubscriptionPlan = lazy(
+  () => import("@/Components/Subscription/SubscriptionPlan")
+);
+const AddNewPlanForm = lazy(
+  () => import("@/Page/admin-dashboard/add-new-plan")
+);
+const UpdatePlanForm = lazy(() => import("@/Page/admin-dashboard/UpdatePlan"));
+const AddNewServiceForm = lazy(
+  () => import("@/Page/admin-dashboard/add-services")
+);
+const ChatPage = lazy(() => import("@/Page/employee-dashboard/ChatPage"));
+const MapRegionOverview = lazy(() => import("@/Page/admin-dashboard/Map"));
 
 export const adminSidebarItems: ISidebarItems[] = [
   {
@@ -17,17 +47,152 @@ export const adminSidebarItems: ISidebarItems[] = [
     title: "Clients",
     item: {
       title: "Clients",
-      url: "/admin/clients",
+      url: "/admin/client",
       component: Clients,
       isActive: false,
     },
   },
   {
-    title: "Help",
+    title: "Invoices",
     item: {
-      title: "Help",
-      url: "/admin-dashboard/help",
-      component: Dashboard,
+      title: "Invoices",
+      url: "/admin/invoices",
+      component: Invoices,
+      isActive: false,
+    },
+  },
+  {
+    title: "Employee Expense Request",
+    item: {
+      title: "Employee Expense Request",
+      url: "/admin/employee-expense-request",
+      component: ExpenseAdmin,
+      isActive: false,
+    },
+  },
+  {
+    title: "Create Invoice",
+    item: {
+      title: "Create Invoice",
+      url: "/admin/create-invoice",
+      component: CreateInvoiceForm,
+      isActive: false,
+    },
+  },
+  {
+    title: "Employees",
+    item: {
+      title: "Employees",
+      url: "/admin/employees",
+      component: Employees,
+      isActive: false,
+    },
+  },
+  {
+    title: "Map",
+    item: {
+      title: "Map",
+      url: "/admin/map",
+      component: MapRegionOverview,
+      isActive: false,
+    },
+  },
+  {
+    title: "Notifications",
+    item: {
+      title: "Notifications",
+      url: "/admin/notifications",
+      component: Notifications,
+      isActive: false,
+    },
+  },
+  {
+    title: "Region",
+    item: {
+      title: "Region",
+      url: "/admin/region",
+      component: Region,
+      isActive: false,
+    },
+  },
+  {
+    title: "Reports",
+    item: {
+      title: "Reports",
+      url: "/admin/reports",
+      component: ReportsPage,
+      isActive: false,
+    },
+  },
+  {
+    title: "Services",
+    item: {
+      title: "Services",
+      url: "/admin/services",
+      component: Services,
+      isActive: false,
+    },
+  },
+  {
+    title: "Settings",
+    item: {
+      title: "Settings",
+      url: "/admin/settings",
+      component: ProfileSettingsPage,
+      isActive: false,
+    },
+  },
+  {
+    title: "Subscriptions",
+    item: {
+      title: "Subscriptions",
+      url: "/admin/subscription",
+      component: Subscriptions,
+      isActive: false,
+    },
+  },
+  {
+    title: "Subscription Plan",
+    item: {
+      title: "Subscription Plan",
+      url: "/admin/subscriptionplan",
+      component: SubscriptionPlan,
+      isActive: false,
+    },
+  },
+  {
+    title: "Add New Plan",
+    item: {
+      title: "Add New Plan",
+      url: "/admin/add-new-plan",
+      component: AddNewPlanForm,
+      isActive: false,
+    },
+  },
+  {
+    title: "Update Plan",
+    item: {
+      title: "Update Plan",
+      url: "/admin/update-plan/:id",
+      component: UpdatePlanForm,
+      isActive: false,
+    },
+  },
+  {
+    title: "Add Services",
+    item: {
+      title: "Add Services",
+      url: "/admin/add-services",
+      component: AddNewServiceForm,
+      isActive: false,
+    },
+  },
+  {
+    title: "Chat",
+    item: {
+      title: "Chat",
+      url: "/admin/chat",
+      component: ChatPage,
       isActive: false,
     },
   },
--- src/Router/sidebar-items/employee-items.tsx
+++ src/Router/sidebar-items/employee-items.tsx
@@ -1,15 +1,125 @@
 import { lazy } from "react";
-const EmployeeDashboard = lazy(() => import("@/Page/employee-dashboard/EmployeeDashboard"))
 import type { ISidebarItems } from "@/Types/Types";
+const RegionDashboard = lazy(
+  () => import("@/Page/employee-dashboard/RegionDashboard")
+);
+const TaskEmployee = lazy(
+  () => import("@/Page/employee-dashboard/TaskEmployee")
+);
+const Buildingregions = lazy(
+  () => import("@/Page/employee-dashboard/Buildingregions")
+);
+const EmployeeDashboard = lazy(
+  () => import("@/Page/employee-dashboard/EmployeeDashboard")
+);
+const EmployeeCommunication = lazy(
+  () => import("@/Page/employee-dashboard/EmployeeCommunication")
+);
+const EmployeeSetting = lazy(
+  () => import("@/Page/employee-dashboard/EmployeeSetting")
+);
+const EmployeeForms = lazy(
+  () => import("@/Page/employee-dashboard/EmployeeForms")
+);
+const EmployeeReports = lazy(
+  () => import("@/Page/employee-dashboard/EmployeeReports")
+);
+const EmployeeInvoicees = lazy(
+  () => import("@/Page/employee-dashboard/invoice/EmployeeInvoicees")
+);
+const EmployeeSubscription = lazy(
+  () => import("@/Page/employee-dashboard/EmployeeSubscription")
+);
 
 export const employeeSidebarItems: ISidebarItems[] = [
   {
     title: "Dashboard",
     item: {
       title: "Dashboard",
-      url: "/employee/dashboard",
+      url: "/employee-dashboard",
       component: EmployeeDashboard,
       isActive: true,
     },
-  }
+  },
+  {
+    title: "Building",
+    item: {
+      title: "Building",
+      url: "/employee-building",
+      component: RegionDashboard,
+      isActive: false,
+    },
+  },
+  {
+    title: "Building Tasks",
+    item: {
+      title: "Building Tasks",
+      url: "/employee-building/:buildingName",
+      component: TaskEmployee,
+      isActive: false,
+    },
+  },
+  {
+    title: "Communication",
+    item: {
+      title: "Communication",
+      url: "/employee-communication",
+      component: EmployeeCommunication,
+      isActive: false,
+    },
+  },
+  {
+    title: "Settings",
+    item: {
+      title: "Settings",
+      url: "/employee-setting",
+      component: EmployeeSetting,
+      isActive: false,
+    },
+  },
+  {
+    title: "Forms",
+    item: {
+      title: "Forms",
+      url: "/employee-forms",
+      component: EmployeeForms,
+      isActive: false,
+    },
+  },
+  {
+    title: "Region",
+    item: {
+      title: "Region",
+      url: "/employee-region",
+      component: Buildingregions,
+      isActive: false,
+    },
+  },
+  {
+    title: "Reports",
+    item: {
+      title: "Reports",
+      url: "/employee-report",
+      component: EmployeeReports,
+      isActive: false,
+    },
+  },
+  {
+    title: "Invoices",
+    item: {
+      title: "Invoices",
+      url: "/employee-invoice",
+      component: EmployeeInvoicees,
+      isActive: false,
+    },
+  },
+  {
+    title: "Subscriptions",
+    item: {
+      title: "Subscriptions",
+      url: "/employee-subscriptions",
+      component: EmployeeSubscription,
+      isActive: false,
+    },
+  },
 ];
